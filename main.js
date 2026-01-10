1b3fde1ac207f72b0dc919eecfe1b00006948ed3
dave <joel.choisea@outlook.de>
2026-01-10 22:18:00 +0100

    update nach bts

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
+gsap.registerPlugin(ScrollTrigger);
 
+const track = document.querySelector(".bts-images-track");
+const btsSection = document.querySelector(".section.bts");
+
+gsap.fromTo(track, 
+    { x: 0, opacity: 1 },
+    {
+        x: () => -(track.scrollWidth + window.innerWidth),
+        ease: "none",
+        scrollTrigger: {
+            trigger: btsSection,
+            start: "top top",
+            end: () => "+=" + track.scrollWidth,
+            scrub: 0.5,
+            pin: true,
+            pinSpacing: true
+        }
+    }
+);
